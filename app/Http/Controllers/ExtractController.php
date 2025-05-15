e97d47acd95e0acc42337b9bd2e545bdf7ea5f08
7acini <guilherme.facini@protonmail.com>
2025-05-15 15:59:31 -0300

    feat(controllers): add controller for processing of transaction and extract

--- app/Http/Controllers/ExtractController.php
+++ app/Http/Controllers/ExtractController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Client;
 class ExtractController extends Controller
 {
-    //
+    public function show($id){
+
+        $client = Client::with(['transaction' => function ($q) {
+            $q->orderBy('created_at','desc')->limit(10);
+        }])->find($id);
+
+        if(!$client){
+            return response()->json(['error' => 'Client not found'],404);
+        }
+
+        return response()->json([
+            'balance' => [
+                'total'=> $client->balance,
+                'limit'=> $client->limit,
+                'extract_date' => now()->toIso8601String(),
+                ],
+                'last_transaction' => $client->transaction
+        ]);
+    }
 }
