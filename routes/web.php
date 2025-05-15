49ccce51a6324211fb39eeb25d22f7d2c8285298
7acini <guilherme.facini@protonmail.com>
2025-05-15 13:50:42 -0300

    feat(routes/web): add routes for endpoints of rinha

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/clientes/{id}/transacoes', [TransacaoController::class, 'store'])
+    ->where(['id' => '[0-9]+']);
+
+Route::get('/clientes/{id}/extrato', [ExtratoController::class, 'show'])
+    ->where(['id' => '[0-9]+']);
